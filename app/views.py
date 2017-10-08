4dfa0313dbe57e876d45d5e3254f51329c01d774
krsnvijay <krsnvijay@gmail.com>
2017-10-08 17:12:15 +0530

    Code Clean Up

--- app/views.py
+++ app/views.py
@@ -1,14 +1,12 @@
 from django.http import Http404
-from django.shortcuts import render
-
 # Create your views here.
 # ViewSets define the view behavior.
-from rest_framework import viewsets, generics
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from app.models import Faculty, Class, Schedule
-from app.serializers import FacultySerializer, ClassSerializer, ScheduleSerializer, AvailabilitySerializer
+from app.serializers import FacultySerializer, ClassSerializer, ScheduleSerializer
 
 
 class FacultyViewSet(viewsets.ModelViewSet):
@@ -43,21 +41,19 @@ class AuthViewSet(APIView):
         faculty = self.get_object(faculty_id, password)
         serializer = FacultySerializer(faculty, context={'request': request})
         return Response(serializer.data)
+
+
 class AvailabiltyViewSet(APIView):
     """
     Retrieve, update or delete a snippet instance.
     """
 
-    def get_object(self, dept, day,hour):
+    def get_object(self, dept, day, hour):
         try:
-            set1 = Faculty.objects.filter(dept=dept,faculty_schedule__day=day, faculty_schedule__hour=hour).values('faculty_id')
-            set2 = Faculty.objects.filter(dept=dept).exclude(faculty_id__in=set1).values('faculty_id','name','phone')
+            set1 = Faculty.objects.filter(dept=dept, faculty_schedule__day=day, faculty_schedule__hour=hour).values(
+                'faculty_id')
+            set2 = Faculty.objects.filter(dept=dept).exclude(faculty_id__in=set1).values('faculty_id', 'name', 'phone')
             return set2
-
-            # return set2.difference(set1).values('faculty_id','faculty_id__name','faculty_id__phone')
-            # set1= Faculty.objects.filter(dept=dept, faculty_schedule__day=day,faculty_schedule__hour =hour)
-            # set2= Faculty.objects.filter(dept=dept)
-            # return set2.difference(set1)
         except Faculty.DoesNotExist:
             raise Http404
 
@@ -65,12 +61,10 @@ class AvailabiltyViewSet(APIView):
         dept = self.request.query_params.get('dept', None)
         day = self.request.query_params.get('day', None)
         hour = self.request.query_params.get('hour', None)
-        availability = self.get_object(dept, day,hour)
-        response={}
-        response['dept']=dept
-        response['day']=day
-        response['hour']=hour
-        response['availability']=availability
+        availability = self.get_object(dept, day, hour)
+        response = {}
+        response['dept'] = dept
+        response['day'] = day
+        response['hour'] = hour
+        response['availability'] = availability
         return Response(response)
-        # serializer = AvailabilitySerializer(availability, context={'request': request})
-        # return Response(serializer.data)
